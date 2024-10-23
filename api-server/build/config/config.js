fdb3b489e772f12aa86d9bc2c46912f35f5b54f7
Kaustav Tarafdar <142386782+KausTarafdar@users.noreply.github.com>
2024-10-24 00:58:28 +0530

    Delete build

--- api-server/build/config/config.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-;
-export const config = {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
-    environment: process.env.DEVELOPMENT ? process.env.development : ''
-};
--- api-server/build/lib/extract.js
+++
@@ -1,9 +0,0 @@
-export default function extractDir(dir) {
-    const re = /.*(?=node_module)/g;
-    let matches = dir.match(re);
-    if (matches !== null) {
-        let processDir = matches[0].split('/').splice(-3).join('/');
-        return processDir;
-    }
-    return dir.split('/').pop() + '/';
-}
